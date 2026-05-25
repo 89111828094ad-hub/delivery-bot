1ce74879b31b032c3a740e4c42062b616c0a2550
89111828094ad-hub <89111828094ad@gmail.com>
2026-05-25 21:29:16 +0300

    Update delivery_bot.py

--- delivery_bot.py
+++ delivery_bot.py
@@ -33,7 +33,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 
 # ─── Настройки ────────────────────────────────────────────────────────────────
 BOT_TOKEN = "8969496219:AAHQnLwzz6IwMI2DZP9DmHs5qdBRr8yVIXU"
-MANAGER_CHAT_IDS = [898299896, 5228371720]
+MANAGER_CHAT_ID = 898299896
 
 WAREHOUSE_ADDRESS = "Санкт-Петербург, ул. Профессора Качалова 8И"
 MANAGER_CONTACT = "📞 +7 966 860 08 11 (Никита)"
